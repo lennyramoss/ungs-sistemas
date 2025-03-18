cf560f39678902fc419d6176e5b9b4b00e1c27c2
Lenny <lenneyramos@gmail.com>
2025-03-18 10:53:33 -0300

    1.3

--- Introduccion a la programacion/ejercicio-17.py
+++ Introduccion a la programacion/ejercicio-17.py
@@ -1,14 +1,15 @@
-dinero = int(input("Ingrese el monto a retirar"))
-diezmil = dinero//10000
-mil = dinero//1000
-quin = dinero//500
-dosc = dinero//200
-cien = dinero //100
-cinc = dinero //50
-vein = dinero//20
-diez = dinero//10
-cinco = dinero//5
-un = dinero//1
+dinero = int(input("Ingrese el monto a retirar "))
+mil = dinero//1000 #billetes de 1000
+rmil = dinero%1000 #lo que me sobra del dinero ingresado
 
-print(diezmil,"billetes de $10000", mil,"billetes de $1000", quin,"billetes de $500", dosc,"billetes de $200", cien,"billetes de $100", cinc,"billete de $50", vein,"billete de $20", diez,"billete de $10", cinc,"billete de $5", un,"moneda de  de $1")
+cien = rmil//100 #divido lo que sobro por 100, dando los billetes de 100 q va a dar
+rcien = rmil%100 #me da el resto de mil divido por 100
+ 
+diez = rcien//10 #divido lo que sobro de 100 por 10
+rdiez = rcien%10 #me da el resto de 100 divido por 10  
 
+uno = rdiez//1 #divido lo que sobro de 100 por 1
+
+print(mil,"billetes de $1000",cien,"billetes de $100",diez,"billetes de $10",uno,"monedas de un $1")
+
+print(rmil)
---
+++ Introduccion a la programacion/ejercicio-18.py
@@ -0,0 +1,10 @@
+dato = int(input("Ingrese la cantidad de segundos "))
+
+dias = dato//86400
+rd = dato%86400
+hs = rd//3600
+rhs = rd%3600
+mins = rhs//60
+rm = rhs%60
+
+print (dias,"Dia",hs,"horas",mins,"minutos",rm,"segundos")
