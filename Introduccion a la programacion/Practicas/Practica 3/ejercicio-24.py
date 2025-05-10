ac63e2dfa539ebe613f55835a9b4d7fafbb1e60f
lenny <lenneyramos@gmail.com>
2025-05-09 21:01:26 -0300

    0.18

---
+++ Introduccion a la programacion/Practicas/Practica 3/ejercicio-24.py
@@ -0,0 +1,16 @@
+#a
+n = int(input("Ingrese un numero: "))
+print ("*"*n)
+
+#b
+n = int(input("Ingrese un numero: "))
+
+for i in range (1,n+1):
+    print("*"*i)
+
+#c
+n = int(input("Ingrese un numero: "))
+
+for i in range (1,n+1):
+    calc= 2*i-1
+    print("*"*calc)
---
+++ Introduccion a la programacion/Practicas/Practica 3/ejercicio.25.py
@@ -0,0 +1,4 @@
+#a
+palabra = str(input("Ingrese una palabra "))
+espacioizq=(255 - len(palabra)) // 2
+print(" "*espacioizq + palabra)
