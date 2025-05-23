78c09ce3345d06ca48503de0294ec70af19b9a04
lenny <lenneyramos@gmail.com>
2025-05-23 20:56:51 -0300

    0.29

--- Introduccion a la programacion/Modelos de parcial/resolucion-2010.py
+++ Introduccion a la programacion/Modelos de parcial/resolucion-2010.py
@@ -12,3 +12,58 @@ def palindromo(a):
 print(palindromo(1578751))
 
 
+#2
+
+import random
+def loteria(listaA,listaB,ganador,valor):
+    ganadores=[]
+    premio=0
+    premioTotal=0
+    total=0
+
+    for i in range(0,len(listaA)):
+        if listaA[i]==ganador:
+            ganadores.append(i)
+    if len(ganadores)==0:
+        print("nadie gano ")
+    elif len(ganadores)==1:
+        premio = (listaB[ganadores[0]]*valor)*1.10
+        print(f"El apostador {ganadores[0]+1} ganó {premio:.2f}$")
+        premioTotal += premio
+    else:
+        for numero in ganadores:
+            print(f"El apostador {numero+1} ganó ${listaB[numero]*valor}$") 
+            premioTotal += listaB[numero]*valor
+
+    for j in range(len(listaB)):
+        total+=listaB[j]
+    if total>premioTotal:
+        print(f"las ganancias de este quinela son {total-premioTotal:.2f}$")
+    elif total==premioTotal:
+        print("este mes no hubo ganancia ")
+    else:
+        print(f"las perdidas de esta quienela son {total-premioTotal:.2f}$")
+
+
+jugadores=[2,15,5,89,2,91,98,89,2,9]
+apostado=[2,2.5,0.5,3,12,5,4.5,5,2.5,3]
+bonus= int(input("Por cuanto se multiplicara el premio "))
+nroGanador=int(input("Ingrese el nmumero ganador "))
+loteria(jugadores,apostado,nroGanador,bonus)
+
+"""while True:
+jugador = int(input("Ingrese el numero que desea jugar "))
+anotados.append(jugador)"""
+
+
+#3
+#from funciones import cadenaC
+
+def repetida(palabra)
+    palabraDesarmada=[]
+    for letra in palabra:
+        if letra in palabraDesarmada:
+            conteo[letra]+=1
+        else:
+            conteo[letra]
+            palabraDesarmada.append(char)
