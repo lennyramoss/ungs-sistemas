c280d95e03821a6d52788ec424e92904f7c567e0
Lenny <lenneyramos@gmail.com>
2025-05-28 04:30:37 -0300

    0.34

--- Introduccion a la programacion/Modelos de parcial/practica-recorrer.py
+++ Introduccion a la programacion/Modelos de parcial/practica-recorrer.py
@@ -12,3 +12,23 @@ No debés contar repeticiones.
 4. Palabra más larga
 Ingresá varias palabras y mostrale al usuario cuál es la más larga de todas.
 """
+
+
+
+#1
+numeros=[1,7,4,2,3,5,7,6]
+AltoNum=0
+for numero in numeros:
+    if numero>AltoNum:
+        AltoNum=numero
+        
+for i in range(len(numeros)):
+    if numeros[i] == AltoNum:# probar que pasa si pongo altonum in numeros
+        print(i)
+
+
+#2
+notas=[6,3,6,7,8,3,2,4,2,1,1,1,1,2,5,10]
+for nota in notas:
+    if nota>=7:
+        print("notas",nota)
